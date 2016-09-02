dcd701e362e61820cc846937f9e66920e7f9a372
Jesse Jenks <jjenks@ups.edu>
2016-09-02 11:42:40 -0700

    Now sorts clubs in alphabetical order
    This way it doesn’t depend on who came and set up first, and it makes
    it easier if you are looking for a specific club

--- data.js
+++ data.js
@@ -1,56 +1,24 @@
 var data = [
-	//[{lat:47.261830, lng:-122.481900},  // 1
+	// Basic format:
+	// [{lat:__, lng:__}, 'html stuff', 'Club name']
+
+	//[{lat:47.261830, lng:-122.481900},
 	//'<div id="content"> <div id="siteNotice"> </div><h1>ACM & WACM</h1><div id="bodyContent"><p>The puget sound chapter of <b>ACM</b>, or the Association for Computing Machinery is a computer science. We also 
 	//have a womens ACM dedicating to promoting women in computing. ACM meets on Sundays from 5pm beginning on Sunday the 11th.</div></div>'],
-/*
-	[{lat:47.261830, lng:-122.481900},  // 1
+//*
+	[{lat:47.261830, lng:-122.481900},
 	'<div id="content"> <div id="siteNotice"> </div><h1>ACM</h1><div id="bodyContent"><p><b>ACM</b> is all about CLUB DESCRIPTION. Enjoy leisurely activities, such as CLUB ACTIVITIES</div></div>',
 	"ACM"],
-	[{lat:47.261765, lng:-122.481900},  // 2
+	[{lat:47.261765, lng:-122.481900},
 	'<div id="content"> <div id="siteNotice"> </div><h1>CLUB NAME</h1><div id="bodyContent"><p><b>CLUB NAME</b> is all about CLUB DESCRIPTION. Enjoy leisurely activities, such as CLUB ACTIVITIES</div> </div>',
-	"CLUB 2"],
-	[{lat:47.261830, lng:-122.481800},  // 3
+	"Bananas"],
+	[{lat:47.261830, lng:-122.481800},
 	'<div id="content"> <div id="siteNotice"> </div><h1>CLUB NAME</h1><div id="bodyContent"><p><b>CLUB NAME</b> is all about CLUB DESCRIPTION. Enjoy leisurely activities, such as CLUB ACTIVITIES</div> </div>',
-	"CLUB 3"], 
-	[{lat:47.261765, lng:-122.481800},  // 4
+	"zebras"], 
+	[{lat:47.261765, lng:-122.481800},
 	'<div id="content"> <div id="siteNotice"> </div><h1>CLUB NAME</h1><div id="bodyContent"><p><b>CLUB NAME</b> is all about CLUB DESCRIPTION. Enjoy leisurely activities, such as CLUB ACTIVITIES</div> </div>',
-	"CLUB 4"], 
-	[{lat:47.261830, lng:-122.481700},  // 5
+	"lions"], 
+	[{lat:47.261830, lng:-122.481700},
 	'<div id="content"> <div id="siteNotice"> </div><h1>CLUB NAME</h1><div id="bodyContent"><p><b>CLUB NAME</b> is all about CLUB DESCRIPTION. Enjoy leisurely activities, such as CLUB ACTIVITIES</div> </div>',
-	"CLUB 5"]//, 
-	/*
-	[{lat:47.261765, lng:-122.481700},  // 6
-	'<div id="content"> <div id="siteNotice"> </div><h1>CLUB NAME</h1><div id="bodyContent"><p><b>CLUB NAME</b> is all about CLUB DESCRIPTION. Enjoy leisurely activities, such as CLUB ACTIVITIES</div> </div>',
-	"CLUB 6"], 
-	[{lat:47.261830, lng:-122.481600},
-	'<div id="content"> <div id="siteNotice"> </div><h1>CLUB NAME</h1><div id="bodyContent"><p><b>CLUB NAME</b> is all about CLUB DESCRIPTION. Enjoy leisurely activities, such as CLUB ACTIVITIES</div> </div>',
-	"CLUB 7"], 
-	[{lat:47.261765, lng:-122.481600},
-	'<div id="content"> <div id="siteNotice"> </div><h1>CLUB NAME</h1><div id="bodyContent"><p><b>CLUB NAME</b> is all about CLUB DESCRIPTION. Enjoy leisurely activities, such as CLUB ACTIVITIES</div> </div>',
-	"CLUB 8"], 
-	[{lat:47.261830, lng:-122.481500},
-	'<div id="content"> <div id="siteNotice"> </div><h1>ACM</h1><div id="bodyContent"><p><b>ACM</b>, also referred to as <b>The BEST Club</b>, is THE BEST CLUB</div> </div>',
-	"ACM"],
-	[{lat:47.261765, lng:-122.481500},
-	'<div id="content"> <div id="siteNotice"> </div><h1>CLUB NAME</h1><div id="bodyContent"><p><b>CLUB NAME</b> is all about CLUB DESCRIPTION. Enjoy leisurely activities, such as CLUB ACTIVITIES</div> </div>',
-	"CLUB 10"],
-	[{lat:47.261830, lng:-122.481400},
-	'<div id="content"> <div id="siteNotice"> </div><h1>CLUB NAME</h1><div id="bodyContent"><p><b>CLUB NAME</b> is all about CLUB DESCRIPTION. Enjoy leisurely activities, such as CLUB ACTIVITIES</div> </div>',
-	"CLUB 11"],
-	[{lat:47.261765, lng:-122.481400},
-	'<div id="content"> <div id="siteNotice"> </div><h1>CLUB NAME</h1><div id="bodyContent"><p><b>CLUB NAME</b> is all about CLUB DESCRIPTION. Enjoy leisurely activities, such as CLUB ACTIVITIES</div> </div>',
-	"CLUB 12"],
-	[{lat:47.261830, lng:-122.481300},
-	'<div id="content"> <div id="siteNotice"> </div><h1>CLUB NAME</h1><div id="bodyContent"><p><b>CLUB NAME</b> is all about CLUB DESCRIPTION. Enjoy leisurely activities, such as CLUB ACTIVITIES</div> </div>',
-	"CLUB 13"],
-	[{lat:47.261765, lng:-122.481300},
-	'<div id="content"> <div id="siteNotice"> </div><h1>CLUB NAME</h1><div id="bodyContent"><p><b>CLUB NAME</b> is all about CLUB DESCRIPTION. Enjoy leisurely activities, such as CLUB ACTIVITIES</div> </div>',
-	"CLUB 14"],
-	[{lat:47.261830, lng:-122.481200},
-	'<div id="content"> <div id="siteNotice"> </div><h1>CLUB NAME</h1><div id="bodyContent"><p><b>CLUB NAME</b> is all about CLUB DESCRIPTION. Enjoy leisurely activities, such as CLUB ACTIVITIES</div> </div>',
-	"CLUB 15"],
-	[{lat:47.261765, lng:-122.481200},
-	'<div id="content"> <div id="siteNotice"> </div><h1>CLUB NAME</h1><div id="bodyContent"><p><b>CLUB NAME</b> is all about CLUB DESCRIPTION. Enjoy leisurely activities, such as CLUB ACTIVITIES</div> </div>',
-	"CLUB 16"]
-	*/
+	"tigers, oh my"]
 ];
