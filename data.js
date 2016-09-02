1742f66bf829f3ecbdb5678320f4c8a75efadc2e
Jesse Jenks <jjenks@ups.edu>
2016-09-02 11:29:32 -0700

    Made it work so it shows a map before people sign up
    If data was empty, the map wouldn’t display, so I just added a few
    checks.

--- data.js
+++ data.js
@@ -2,7 +2,7 @@ var data = [
 	//[{lat:47.261830, lng:-122.481900},  // 1
 	//'<div id="content"> <div id="siteNotice"> </div><h1>ACM & WACM</h1><div id="bodyContent"><p>The puget sound chapter of <b>ACM</b>, or the Association for Computing Machinery is a computer science. We also 
 	//have a womens ACM dedicating to promoting women in computing. ACM meets on Sundays from 5pm beginning on Sunday the 11th.</div></div>'],
-//*
+/*
 	[{lat:47.261830, lng:-122.481900},  // 1
 	'<div id="content"> <div id="siteNotice"> </div><h1>ACM</h1><div id="bodyContent"><p><b>ACM</b> is all about CLUB DESCRIPTION. Enjoy leisurely activities, such as CLUB ACTIVITIES</div></div>',
 	"ACM"],
