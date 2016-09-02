ab90b7b0aa263485ec8ac74c6acf297462a9742b
Jesse Jenks <jjenks@ups.edu>
2016-09-02 12:22:53 -0700

    small changes

--- data.js
+++ data.js
@@ -5,7 +5,9 @@ var data = [
 	//[{lat:47.261830, lng:-122.481900},
 	//'<div id="content"> <div id="siteNotice"> </div><h1>ACM & WACM</h1><div id="bodyContent"><p>The puget sound chapter of <b>ACM</b>, or the Association for Computing Machinery is a computer science. We also 
 	//have a womens ACM dedicating to promoting women in computing. ACM meets on Sundays from 5pm beginning on Sunday the 11th.</div></div>'],
-//*
+	
+	//test clubs
+/*
 	[{lat:47.261830, lng:-122.481900},
 	'<div id="content"> <div id="siteNotice"> </div><h1>ACM</h1><div id="bodyContent"><p><b>ACM</b> is all about CLUB DESCRIPTION. Enjoy leisurely activities, such as CLUB ACTIVITIES</div></div>',
 	"ACM"],
@@ -21,4 +23,5 @@ var data = [
 	[{lat:47.261830, lng:-122.481700},
 	'<div id="content"> <div id="siteNotice"> </div><h1>CLUB NAME</h1><div id="bodyContent"><p><b>CLUB NAME</b> is all about CLUB DESCRIPTION. Enjoy leisurely activities, such as CLUB ACTIVITIES</div> </div>',
 	"tigers, oh my"]
+	*/
 ];
