1430b4286ea0d2e5b72a94f3411952bde0abbabe
Jesse Jenks <jjenks@ups.edu>
2016-09-01 19:24:29 -0700

    InfoWindows will open when a link is clicked on!
    If we could either sort alphabetically, or automate the name in the
    link, that would be great

--- data.js
+++ data.js
@@ -1,8 +1,12 @@
 var data = [
+	//[{lat:47.261830, lng:-122.481900},  // 1
+	//'<div id="content"> <div id="siteNotice"> </div><h1>ACM & WACM</h1><div id="bodyContent"><p>The puget sound chapter of <b>ACM</b>, or the Association for Computing Machinery is a computer science. We also 
+	//have a womens ACM dedicating to promoting women in computing. ACM meets on Sundays from 5pm beginning on Sunday the 11th.</div></div>'],
+//*
 	[{lat:47.261830, lng:-122.481900},  // 1
 	'<div id="content"> <div id="siteNotice"> </div><h1>CLUB NAME</h1><div id="bodyContent"><p><b>CLUB NAME</b> is all about CLUB DESCRIPTION. Enjoy leisurely activities, such as CLUB ACTIVITIES</div></div>'],
 	[{lat:47.261765, lng:-122.481900},  // 2
-	'<div id="content"> <div id="siteNotice"> </div><h1>CLUB NAME</h1><div id="bodyContent"><p><b>CLUB NAME</b> is all about CLUB DESCRIPTION. Enjoy leisurely activities, such as CLUB ACTIVITIES</div> </div>'], 
+	'<div id="content"> <div id="siteNotice"> </div><h1>CLUB NAME</h1><div id="bodyContent"><p><b>CLUB NAME</b> is all about CLUB DESCRIPTION. Enjoy leisurely activities, such as CLUB ACTIVITIES</div> </div>'],
 	[{lat:47.261830, lng:-122.481800},  // 3
 	'<div id="content"> <div id="siteNotice"> </div><h1>CLUB NAME</h1><div id="bodyContent"><p><b>CLUB NAME</b> is all about CLUB DESCRIPTION. Enjoy leisurely activities, such as CLUB ACTIVITIES</div> </div>'], 
 	[{lat:47.261765, lng:-122.481800},  // 4
@@ -31,4 +35,5 @@ var data = [
 	'<div id="content"> <div id="siteNotice"> </div><h1>CLUB NAME</h1><div id="bodyContent"><p><b>CLUB NAME</b> is all about CLUB DESCRIPTION. Enjoy leisurely activities, such as CLUB ACTIVITIES</div> </div>'],
 	[{lat:47.261765, lng:-122.481200},
 	'<div id="content"> <div id="siteNotice"> </div><h1>CLUB NAME</h1><div id="bodyContent"><p><b>CLUB NAME</b> is all about CLUB DESCRIPTION. Enjoy leisurely activities, such as CLUB ACTIVITIES</div> </div>']
+	//*/
 ];
