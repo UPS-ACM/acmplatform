8a0a880db0c03e70bc66a016c032fab9d1aff069
Thavy Thach <thavythach@gmail.com>
2016-09-02 17:08:11 -0700

    3

--- data.js
+++ data.js
@@ -8,16 +8,21 @@ var data = [
 	
 	//test clubs
 
-	// [{lat:47.261830, lng:-122.481900},
-	// '<div id="content"> <div id="siteNotice"> </div><h1>ACM</h1><div id="bodyContent"><p><b>ACM</b> is all about CLUB DESCRIPTION. Enjoy leisurely activities, such as CLUB ACTIVITIES</div></div>',
-	// "ACM"],
-	// [{lat:47.261765, lng:-122.481900},
-	// '<div id="content"> <div id="siteNotice"> </div><h1>CLUB NAME</h1><div id="bodyContent"><p><b>CLUB NAME</b> is all about CLUB DESCRIPTION. Enjoy leisurely activities, such as CLUB ACTIVITIES</div> </div>',
-	// "Bananas"],
+	[{lat:47.26252533938278, lng:-122.48101741075516 },
+	'<div id="content"> <div id="siteNotice"> </div><h1>Peer Advisors</h1><div id="bodyContent"><p><b>Peer Advisors</b> Come by to ask any questions about academic advising.</div></div>',"Peer Advisors"],
+	[{lat:47.26252897989237, lng:-122.48108983039856 },
+	'<div id="content"> <div id="siteNotice"> </div><h1>RDG</h1><div id="bodyContent"><p><b>RDG</b> The largest club on campus. A dance group who have danced their whole lives or never at all. Open to everyone.</div></div>',"RDG"],
+
+	[{lat:47.26221589515171, lng:-122.4810978770256},
+	'<div id="content"> <div id="siteNotice"> </div><h1>UPS Intervasity</h1><div id="bodyContent"><p><b>UPS Intervarsity</b> Come Kick it w/ us for some key chains doe.</div> </div>',"UPS Intervarsity"]
+
+	[{lat:47.26221589515171, lng:-122.4810978770256},
+	'<div id="content"> <div id="siteNotice"> </div><h1>UPS Intervasity</h1><div id="bodyContent"><p><b>UPS Intervarsity</b> Come Kick it w/ us for some key chains doe.</div> </div>',"UPS Intervarsity"]
+	
 	// [{lat:47.261830, lng:-122.481800},
 	// '<div id="content"> <div id="siteNotice"> </div><h1>CLUB NAME</h1><div id="bodyContent"><p><b>CLUB NAME</b> is all about CLUB DESCRIPTION. Enjoy leisurely activities, such as CLUB ACTIVITIES</div> </div>',
 	// "zebras"], 
 	// [{lat:47.261765, lng:-122.481800},
 	// '<div id="content"> <div id="siteNotice"> </div><h1>CLUB NAME</h1><div id="bodyContent"><p><b>CLUB NAME</b> is all about CLUB DESCRIPTION. Enjoy leisurely activities, such as CLUB ACTIVITIES</div> </div>',
 	// "lions"]
-	
+	]
